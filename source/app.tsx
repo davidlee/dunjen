2d55106ee0cea6a5e27193269ead8f6e5cb21d3d
David Lee <dav@davlee.com>
2023-07-20 19:46:47 +1000

    tabs to spaces

--- source/app.tsx
+++ source/app.tsx
@@ -5,14 +5,14 @@ const width : number = 120;
 const height : number = 40;
 
 type Props = {
-	name: string | undefined;
+  name: string | undefined;
 };
 
 export default function App({}: Props) {
   // ...
   
-	return (
-		<Box key="foo" flexDirection="column" flexWrap="wrap" height={height}>
+  return (
+    <Box key="foo" flexDirection="column" flexWrap="wrap" height={height}>
       {Array.from({ length: height }, (_, y) => (
         <Box key={'y-'+y}>
           {Array.from({ length: width }, (_, x) => (
@@ -21,6 +21,6 @@ export default function App({}: Props) {
         </Box>
       ))}
     </Box>
-	);
+  );
 }
 

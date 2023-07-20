649fec9185ea86d411ab3d68927dc01df76be4a0
David Lee <dav@davlee.com>
2023-07-20 19:15:24 +1000

    more idiomatic tsx layout

--- source/app.tsx
+++ source/app.tsx
@@ -2,27 +2,25 @@ import React from 'react';
 import {Text, Box} from 'ink';
 
 const width : number = 120;
-const height : number = 100;
+const height : number = 40;
 
 type Props = {
 	name: string | undefined;
 };
 
 export default function App({}: Props) {
-  let grid : String[][] = [];
-  let y, x : number;
-
-  for(y = 0; y < height; y++) {
-    let row : String[] = [];
-    grid.push(row);
-    // row.push(<Box>);  
-    for(x = 0; x < width; x++) {
-      row.push('.');
-    }
-    // row.push(</Box>);
-  }
+  // ...
+  
 	return (
-		<Box>{grid.map( e => <Text>{e}</Text>)}</Box>
+		<Box key="foo" flexDirection="column" flexWrap="wrap" height={height}>
+      {Array.from({ length: height }, (_, y) => (
+        <Box key={'y-'+y}>
+          {Array.from({ length: width }, (_, x) => (
+            <Text key={y + ',' + x}>.</Text>
+          ))}
+        </Box>
+      ))}
+    </Box>
 	);
 }
 

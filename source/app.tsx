7c17e661e6366891214cc90532f6f190716231ea
David Lee <dav@davlee.com>
2023-07-21 09:15:28 +1000

    feat: add counter; refactor tsx

--- source/app.tsx
+++ source/app.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {Text, Box} from 'ink';
+
+// import useStdin from 'ink';
+// const {stdin} = useStdin();
 
 const width : number = 120;
 const height : number = 40;
@@ -8,11 +11,27 @@ type Props = {
   name: string | undefined;
 };
 
+const Counter = () => {
+	const [counter, setCounter] = useState(0);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setCounter(previousCounter => previousCounter + 1);
+		}, 100);
+
+		return () => {
+			clearInterval(timer);
+		};
+	}, []);
+
+	return <Text color="green">tick, tock ... {counter}</Text>;
+};
+
 export default function App({}: Props) {
   // ...
   
   return (
-    <Box key="foo" flexDirection="column" flexWrap="wrap" height={height}>
+    <Box key="app-wrapper" flexDirection="column" flexWrap="wrap" height={height}>
       {Array.from({ length: height }, (_, y) => (
         <Box key={'y-'+y}>
           {Array.from({ length: width }, (_, x) => (
@@ -20,7 +39,11 @@ export default function App({}: Props) {
           ))}
         </Box>
       ))}
+      <Counter />
     </Box>
   );
 }
+
+
+
 

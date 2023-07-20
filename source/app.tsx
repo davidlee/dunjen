d5e19812a527e5e416b2ed170f777c336081b7d8
David Lee <dav@davlee.com>
2023-07-21 09:49:36 +1000

    prettier.

--- source/app.tsx
+++ source/app.tsx
@@ -1,49 +1,50 @@
-import React, {useState, useEffect} from 'react';
-import {Text, Box} from 'ink';
+import React, { useState, useEffect } from "react"
+import { Text, Box } from "ink"
 
 // import useStdin from 'ink';
 // const {stdin} = useStdin();
 
-const width : number = 120;
-const height : number = 40;
+const width: number = 120
+const height: number = 40
 
 type Props = {
-  name: string | undefined;
-};
+  name: string | undefined
+}
 
 const Counter = () => {
-	const [counter, setCounter] = useState(0);
+  const [counter, setCounter] = useState(0)
 
-	useEffect(() => {
-		const timer = setInterval(() => {
-			setCounter(previousCounter => previousCounter + 1);
-		}, 100);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCounter((previousCounter) => previousCounter + 1)
+    }, 100)
 
-		return () => {
-			clearInterval(timer);
-		};
-	}, []);
+    return () => {
+      clearInterval(timer)
+    }
+  }, [])
 
-	return <Text color="green">tick, tock ... {counter}</Text>;
-};
+  return <Text color="green">tick, tock ... {counter}</Text>
+}
 
 export default function App({}: Props) {
   // ...
-  
+
   return (
-    <Box key="app-wrapper" flexDirection="column" flexWrap="wrap" height={height}>
+    <Box
+      key="app-wrapper"
+      flexDirection="column"
+      flexWrap="wrap"
+      height={height}
+    >
       {Array.from({ length: height }, (_, y) => (
-        <Box key={'y-'+y}>
+        <Box key={"y-" + y}>
           {Array.from({ length: width }, (_, x) => (
-            <Text key={y + ',' + x}>.</Text>
+            <Text key={y + "," + x}>.</Text>
           ))}
         </Box>
       ))}
       <Counter />
     </Box>
-  );
+  )
 }
-
-
-
-
